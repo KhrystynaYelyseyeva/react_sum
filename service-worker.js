42122781059d914ca56eea1e65ad6c0387f8f66f
KhrystynaYelyseyeva <play1409@gmail.com>
2020-10-21 10:57:21 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.0be2a4d51bf87742479c1b1baf41cd4d.js"
+  "./precache-manifest.65c28644aa27760f18769d866489bc98.js"
 );
 
 self.addEventListener('message', (event) => {
